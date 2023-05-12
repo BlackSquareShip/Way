aed593f20f8808836e4bd2b999b8fb393055785a
BlackSquareShip <danilg2002@mail.ru>
2023-05-12 09:39:36 +0200

    change intro bg

--- src/components/SecondBlock/Intro.tsx
+++ src/components/SecondBlock/Intro.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Intro() {
     return(
-        <div className={"bg-cover bg-no-repeat introBg h-[600px]"}>
+        <div className={"bg-cover bg-no-repeat bg-[url('../public/trafic.jpeg')] introBg h-[600px]"}>
             <div className={"h-full bg-black/50"}>
                 <div className={"flex w-1/2 m-auto flex-col h-full justify-center text-3xl font-bold"}>
                     Путь - это проект, который призван уменьшить количество трафика на дорогах, сплотив людей, и дав им возможность находить компанию в дорогу до работы.
