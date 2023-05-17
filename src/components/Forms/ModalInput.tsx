07ce377cdd1841836c25ec1bbaee098d8d212190
BlackSquareShip <danilg2002@mail.ru>
2023-05-17 17:51:48 +0200

    change by aleksey

--- src/components/Forms/ModalInput.tsx
+++ src/components/Forms/ModalInput.tsx
@@ -1,17 +1,24 @@
-interface inputProps{
-    inputId: string,
-    inputType: string,
-    labelText: string,
+import React, {ChangeEvent} from "react";
+
+interface InputProps {
+    inputId: string
+    value: string
+    labelText: string
     name: string
-    handleChange: (event : React.FormEvent) => void
+    type: string
+    handleChange: (e: ChangeEvent<HTMLInputElement>, type: string) => void
 }
 
-export function ModalInput({inputId, inputType, labelText, name, handleChange} : inputProps){
-    return(
+export function ModalInput({inputId, value,name, labelText, type, handleChange}: InputProps) {
+    return (
         <div className="flex flex-col w-5/6 m-auto">
-            <label htmlFor={inputId}  className="text-black text-2xl font-bold">{labelText}</label>
-            <input  type={inputType} name={name}  id={inputId} onChange={handleChange}
-            className="pl-3 text-gray-700 font-bold text-xl bg-[#ECECEC] h-14 rounded-xl border-4 border-white outline-none" />
+            <label htmlFor={inputId} className="text-black text-2xl font-bold">{labelText}</label>
+            <input type={type} name={inputId} id={inputId}
+                   className="pl-3 text-gray-700 font-bold text-xl bg-[#ECECEC]
+                     h-14 rounded-xl border-4 border-white outline-none"
+                   onChange={(e) => handleChange(e, name)}
+                   value={value}
+            />
         </div>
     )
 }
--- src/components/Forms/RegistrationForm.tsx
+++ src/components/Forms/RegistrationForm.tsx
@@ -1,51 +1,50 @@
-import { useState } from "react";
-import { ModalInput } from "./ModalInput";
-import { IUser } from "../../models/User";
+import React, {ChangeEvent, useState} from "react";
+import {ModalInput} from "./ModalInput";
+import {IUser} from "../../models/User";
 import axios from 'axios';
 
-const User: IUser= {
+const User = {
     fio: "",
     email: "",
     password: ""
 }
 
+export function RegistrationForm() {
+    const [formData, setFormData] = useState<IUser>(User);
+    console.log(formData)
 
-
-export function RegistrationForm(){
-    const [state, setState] = useState({User});
-
-    const handleSubmit = (event : React.FormEvent) => {
+    const handleSubmit = (event: React.FormEvent) => {
         event.preventDefault();
-        
-        const response = axios.post<IUser>("http://localhost:3030", User)
+
+        // const response = axios.post<IUser>("http://localhost:3030/users/createUser", state).then()
 
         // onCreate((await response).data)
     };
-  
-    const handleChange = (event : any) => {
-      const fio = event.target.fio;
-      const email = event.target.email;
-      const password = event.target.password;
-      User.fio = fio
-      User.email = email
-      User.password = password
-      setState({User});
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>, field: string) => {
+
+        setFormData(prev => ({...prev, [field]: event.target.value}))
+
     };
-  
+
 
     return (
-        <form className="w-full h-5/6 flex flex-col mb-10">
+        <form className="w-full h-5/6 flex flex-col mb-10" onSubmit={handleSubmit}>
 
-            <ModalInput inputId="fioField" inputType="text" labelText="ФИО" name="fio" handleChange={handleChange}/>
+            <ModalInput inputId="fioField" name='fio' value={formData.fio} labelText="ФИО" type="text"
+                        handleChange={handleChange}/>
 
-            <ModalInput inputId="loginField" inputType="text" labelText="Email" name="email"  handleChange={handleChange}/>
+            {/*<ModalInput inputId="loginField" inputType="text" labelText="Email" name="email"*/}
+            {/*            handleChange={handleChange}/>*/}
 
-            <ModalInput inputId="passwordField" inputType="password" labelText="Пароль" name="password"  handleChange={handleChange}/>
+            {/*<ModalInput inputId="passwordField" inputType="password" labelText="Пароль" name="password"*/}
+            {/*            handleChange={handleChange}/>*/}
 
-            <ModalInput inputId="passwordConfirmField" inputType="password" labelText="Подтвердите пароль" name=""  handleChange={handleChange}/>
+            {/*<ModalInput inputId="passwordConfirmField" inputType="password" labelText="Подтвердите пароль" name=""*/}
+            {/*            handleChange={handleChange}/>*/}
 
-            <input type="submit" value="Зарегистрироваться" onSubmit={handleSubmit}
-                className="cursor-pointer w-1/2 m-auto text-gray-700 font-bold text-2xl bg-[#ECECEC] h-16 rounded-2xl border-[4px] border-black
+            <input type="submit" value="Зарегистрироваться"
+                   className="cursor-pointer w-1/2 m-auto text-gray-700 font-bold text-2xl bg-[#ECECEC] h-16 rounded-2xl border-[4px] border-black
                 hover:bg-white hover:text-black"/>
             <a className="text-black m-auto mb-2" href="">У меня уже есть аккаунт</a>
         </form>
